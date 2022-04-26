1aacd7db588892515b03d8e06871960cc369226e
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2022-04-26 23:21:07 +0300

    Tutorial-50-1.16.5

---
+++ src/main/java/net/project/project/tileentity/VerstakTile.java
@@ -0,0 +1,145 @@
+package net.project.project.tileentity;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.tileentity.ITickableTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.Direction;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+import net.project.project.data.recipes.ModRecipeTypes;
+import net.project.project.data.recipes.VerstakRecipe;
+import net.project.project.item.ModItems;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Optional;
+
+public class VerstakTile extends TileEntity implements ITickableTileEntity {
+
+    private final ItemStackHandler itemHandler = createHandler();
+    private final LazyOptional<IItemHandler> handler = LazyOptional.of(() -> itemHandler);
+
+    public VerstakTile(TileEntityType<?> tileEntityTypeIn) {
+        super(tileEntityTypeIn);
+    }
+
+    public VerstakTile() {
+        this(ModTileEntities.VERSTAK_TILE.get());
+    }
+
+    @Override
+    public void read(BlockState state, CompoundNBT nbt) {
+        itemHandler.deserializeNBT(nbt.getCompound("inv"));
+        super.read(state, nbt);
+    }
+
+    @Override
+    public CompoundNBT write(CompoundNBT compound) {
+        compound.put("inv", itemHandler.serializeNBT());
+        return super.write(compound);
+    }
+
+    private ItemStackHandler createHandler() {
+        return new ItemStackHandler(2) {
+            @Override
+            protected void onContentsChanged(int slot) {
+                markDirty();
+            }
+
+            @Override
+            public boolean isItemValid(int slot, @Nonnull ItemStack stack) {
+                        return true;
+
+            }
+
+            @Override
+            public int getSlotLimit(int slot) {
+                return 1;
+            }
+
+            @Nonnull
+            @Override
+            public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
+                if(!isItemValid(slot, stack)) {
+                    return stack;
+                }
+
+                return super.insertItem(slot, stack, simulate);
+            }
+        };
+    }
+
+    @Nonnull
+    @Override
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            return handler.cast();
+        }
+
+        return super.getCapability(cap, side);
+    }
+
+    private void strikeLightning() {
+        if(!this.world.isRemote()) {
+            EntityType.LIGHTNING_BOLT.spawn((ServerWorld)world, null, null,
+                    pos, SpawnReason.TRIGGERED, true, true);
+        }
+    }
+
+    public void craft() {
+        Inventory inv = new Inventory(itemHandler.getSlots());
+        for (int i = 0; i < itemHandler.getSlots(); i++) {
+            inv.setInventorySlotContents(i, itemHandler.getStackInSlot(i));
+        }
+
+        Optional<VerstakRecipe> recipe = world.getRecipeManager()
+                .getRecipe(ModRecipeTypes.VERSTAK_RECIPE, inv, world);
+
+        recipe.ifPresent(iRecipe -> {
+            ItemStack output = iRecipe.getRecipeOutput();
+
+            if(iRecipe.getWeather().equals(VerstakRecipe.Weather.CLEAR) &&
+                    !world.isRaining()) {
+                craftTheItem(output);
+            }
+
+            if(iRecipe.getWeather().equals(VerstakRecipe.Weather.RAIN) &&
+                    world.isRaining()) {
+                craftTheItem(output);
+            }
+
+            if(iRecipe.getWeather().equals(VerstakRecipe.Weather.THUNDERING) &&
+                    world.isThundering()) {
+                strikeLightning();
+                craftTheItem(output);
+            }
+
+            markDirty();
+        });
+    }
+
+    private void craftTheItem(ItemStack output) {
+        itemHandler.extractItem(0, 1, false);
+        itemHandler.extractItem(1, 1, false);
+        itemHandler.insertItem(1, output, false);
+    }
+
+    @Override
+    public void tick() {
+        if(world.isRemote)
+            return;
+
+        craft();
+    }
+}
